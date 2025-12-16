649bcd7130a154136ce06e60d9664cc94ec0cd1e
Giri <giripola66@gmail.com>
2025-12-16 18:51:11 +0700

    update

--- src/logic/useLocations.js
+++ src/logic/useLocations.js
@@ -3,6 +3,10 @@ import Kangen2 from "../img/kangen2.JPG";
 import PhotoBox from "../img/PhotoBox.JPG";
 import video1 from "../img/video1.mp4";
 import video2 from "../img/video2.MOV";
+import bakmie1 from "../img/bakmie1.jpeg"
+import bakmie2 from "../img/bakmie2.jpeg"
+import bakmie3 from "../img/bakmie3.mp4"
+import bakmie4 from "../img/bakmie4.mp4"
 
 export const useLocations = () => {
     return [
@@ -12,9 +16,9 @@ export const useLocations = () => {
             videos: [video1, video2]
         },
         {
-            name: "Next?",
-            photos: ["/img/mali1.jpg", "/img/mali2.jpg"],
-            videos: []
+            name: "Bakmie Ayam 58",
+            photos: [bakmie1, bakmie2],
+            videos: [bakmie3, bakmie4]
         },
     ];
 };
